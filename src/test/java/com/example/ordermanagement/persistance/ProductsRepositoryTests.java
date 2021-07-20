5a86309cfacc315095a9e47e26987620fa238991
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-07-21 00:23:26 +0900

    feat : Springboot mybatis 연동 및 Test

--- src/test/java/com/example/ordermanagement/persistance/ProductsRepositoryTests.java
+++ src/test/java/com/example/ordermanagement/persistance/ProductsRepositoryTests.java
@@ -2,22 +2,32 @@ package com.example.ordermanagement.persistance;
 
 import com.example.ordermanagement.domain.Products;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 @Slf4j
 public class ProductsRepositoryTests {
 
-    @Autowired
+    @Mock
     private ProductsRepository productsRepository;
 
     @Test
+    @DisplayName("모든 제품을 아이디 순서대로 불러온다.")
     public void getProducts(){
         List<Products> products = productsRepository.getProducts();
-        log.info("{}",products);
+        int count=1;
+        for(Products product : products){
+            Assertions.assertEquals(product.getProdId(),count++);
+            Assertions.assertEquals(product.getProdPrice(),any());
+            Assertions.assertEquals(product.getProdName(),any());
+        }
     }
 }
