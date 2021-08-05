f87b27981732bb817893e4e6aa78708d8da43988
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-08-05 14:00:33 +0900

    feat : addressApiService logic 설계 및 Test

--- src/test/java/com/example/ordermanagement/presentation/apiController/AddressApiControllerTests.java
+++ src/test/java/com/example/ordermanagement/presentation/apiController/AddressApiControllerTests.java
@@ -1,15 +1,18 @@
 package com.example.ordermanagement.presentation.apiController;
 
 
+import com.example.ordermanagement.application.service.AddressApiService;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.verify;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
@@ -20,10 +23,16 @@ public class AddressApiControllerTests {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private AddressApiService addressApiService;
+
     @Test
     @DisplayName("서버에서 요청에 성공한다.")
     public void findById() throws Exception {
         mvc.perform(get("/address/1"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("")
+                ));
     }
 }
