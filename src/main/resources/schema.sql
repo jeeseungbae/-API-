2cf78f2d62e81a9de90f61a4c784c69ac3a8a5f3
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-08-23 14:27:03 +0900

    feat : 데이터베이스 테이블 설계

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,27 @@
-DROP TABLE IF EXISTS address;
+DROP TABLE IF EXISTS address,oauth,user;
 
 create table address(
     seq bigint(255) not null auto_increment primary key,
     content varchar(255) not null,
     distinction int(255) not null
+);
+
+create table oauth(
+    seq bigint(255) not null auto_increment primary key,
+    name varchar(20) not null
+);
+
+create table user(
+    seq bigint(255) not null auto_increment primary key,
+    id varchar(30) not null,
+    password varchar(30) not null,
+    name varchar(30) not null,
+    nickname varchar(20) not null,
+    birthday varchar(10) not null,
+    phone_number varchar(13) not null,
+    email varchar(80) not null,
+    address varchar(255) not null,
+    role int(1) not null,
+    grade int(1) not null,
+    registered_at date not null
 );
