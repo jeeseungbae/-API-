947d8341ea6170150c46abdb39b2c30b85f7af69
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-08-24 20:58:18 +0900

    style : 문맥 및 naming 변경

--- src/main/java/com/example/ordermanagement/persistance/repository/CustomerApiRepository.java
+++ src/main/java/com/example/ordermanagement/persistance/repository/CustomerApiRepository.java
@@ -2,8 +2,10 @@ package com.example.ordermanagement.persistance.repository;
 
 import com.example.ordermanagement.domain.model.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 
 public interface CustomerApiRepository extends JpaRepository<Customer,Long> {
 
